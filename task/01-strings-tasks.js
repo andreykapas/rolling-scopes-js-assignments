1559350ac71c03f2d72c3365c2d7cfc16d333280
andreykapas <andreykapas@gmail.com>
2016-03-17 17:40:15 +0300

    implementing extractNameFromTemplate Fix #1

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -69,6 +69,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
+    // closing
     return value.slice(7, -1);
 }
 
