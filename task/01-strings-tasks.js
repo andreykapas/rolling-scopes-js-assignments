3e053e415624337bf9e7149533466b5f3218581a
andreykapas <andreykapas@gmail.com>
2016-03-17 17:32:21 +0300

    implementing extractNameFromTemplate Fix#1

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -69,7 +69,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    return value.slice(7, value.length - 1);
+    return value.slice(7, -1);
 }
 
 
@@ -229,6 +229,7 @@ function getRectangleString(width, height) {
  */
 function encodeToRot13(str) {
     throw new Error('Not implemented');
+
 }
 
 /**
