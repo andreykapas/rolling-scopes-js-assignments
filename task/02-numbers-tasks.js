95145b3246c3f5d1d1fe54476e074108bc4a0986
andreykapas <andreykapas@gmail.com>
2016-03-18 19:25:49 +0300

    Fix #6

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -112,7 +112,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-    return Math.acos(((x1*x2)+(y1*y2))/(Math.sqrt(x1*x1 + y1*y1) * Math.sqrt(x2*x2 + y2*y2)));
+    //return Math.acos(((x1*x2)+(y1*y2))/(Math.sqrt(x1*x1 + y1*y1) * Math.sqrt(x2*x2 + y2*y2)));
+    return Math.acos(((x1*x2)+(y1*y2))/(Math.hypot(x1, y1) * Math.hypot(x2, y2)));
 }
 
 /**
