9aa48cca52a07c4201a12a7a3ea6d54a6f774307
andreykapas <andreykapas@gmail.com>
2016-03-18 19:19:17 +0300

    Fix #8

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -57,14 +57,15 @@ function parseDataFromIso8601(value) {
  */
 function isLeapYear(date) {
     let year = date.getFullYear();
-    if( (year % 4 != 0)){
+   /* if( (year % 4 != 0)){
         return false;
     } else if (year % 100 != 0) {
         return true;
     } else if (year % 400 != 0){
         return false;
     } else
-        return true;
+        return true;*/
+    return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
 }
 
 
