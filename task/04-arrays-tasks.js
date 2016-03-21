896b81a3ce82eeb38b01f22fe9e186cc89f76f9c
andreykapas <andreykapas@gmail.com>
2016-03-21 16:38:32 +0300

    implementing getTail

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -170,7 +170,6 @@ function insertItem(arr, item, index) {
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
 function getHead(arr, n) {
-   //throw new Error('Not implemented');
    return arr.splice(0, n, Number.MAX_VALUE);
 }
 
@@ -186,7 +185,7 @@ function getHead(arr, n) {
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
 function getTail(arr, n) {
-   throw new Error('Not implemented');
+   return arr.splice(-n, Number.MAX_VALUE);
 }
 
 
