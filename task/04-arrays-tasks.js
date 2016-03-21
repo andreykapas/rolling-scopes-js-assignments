a56be18ca47d49a907f4505985fab3300f2b6714
andreykapas <andreykapas@gmail.com>
2016-03-21 15:23:23 +0300

    delete unnecessary return from  insertItem

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -156,7 +156,7 @@ function getStringsLength(arr) {
  *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
  */
 function insertItem(arr, item, index) {
-   return arr.splice(index, 0, item);
+   arr.splice(index, 0, item);
 }
 
 /**
